e5ae40d3fe59ad99393922b2e39b0358e32b0669
Hardik Singh <singhhardy724@gmail.com>
2025-05-29 00:07:07 +0530

    minor fix

--- index.js
+++ index.js
@@ -47,5 +47,5 @@ app.post('/send-email', async (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running`);
 });
