84b2aad23eb2751d596595a24b691bfcc9f9517d
Hardik Singh <singhhardy724@gmail.com>
2025-05-28 23:54:44 +0530

    vercel and cors fix

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const cors = require('cors');
-app.use(cors());
-app.options('*', cors());
+
+app.use(cors({ origin: '*' }));
+app.options('*', cors({ origin: '*' }));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
